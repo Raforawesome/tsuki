f5b8cc089474d2145a85a3fe804166ee59e86b14
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-11-22 16:04:36 -0500

    Separate filesystem Rust backend by platform

--- src/api.rs
+++ src/api.rs
@@ -1,2 +1,10 @@
-pub mod fs;
+#[cfg(unix)]
+pub mod fs_unix;
+#[cfg(windows)]
+pub mod fs_windows;
+#[cfg(unix)]
+pub use fs_unix as fs;
+#[cfg(windows)]
+pub use fs_windows as fs;
+
 pub mod tsuki_dbg;
