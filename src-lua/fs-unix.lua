976455b1376b346c9039433656583e06cf4ca1c8
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-11-25 02:48:21 -0500

    Add read-only proxy for `fs.Path.segments` field

--- src-lua/fs-unix.lua
+++ src-lua/fs-unix.lua
@@ -4,9 +4,9 @@ local fs = {}
 ---@field name string
 fs.File = {}
 
-
 ---@class Path
 ---@field private segments string[]
+---@field private segments_readonly_ string[]
 ---# tsuki.fs.Path
 ---A class to represent file paths and allow
 ---ergonomic path traversal.
@@ -20,8 +20,15 @@ fs.Path = {}
 ---local home_path = Path.new("/home/arch/projects/tsuki")
 ---```
 function fs.Path.new(from)
+    local split = FS_INTERNAL.split_path(from)
     local new_path = setmetatable({
-        segments = FS_INTERNAL.split_path(from)
+        segments = split,
+        segments_readonly_ = setmetatable({}, {
+            __index = split,
+            __newindex = function()
+                error("Attempt to modify read-only `Path.segments`")
+            end
+        })
     }, {
         __index = fs.Path,          -- Inherit methods from Path
         __newindex = function() end -- Disable adding new behaviour
@@ -49,7 +56,7 @@ end
 ---end           -- "home", "arch", "luafile"
 ---```
 function fs.Path:get_segments()
-    return self.segments
+    return self.segments_readonly_
 end
 
 return fs
